66cc52a11fada90b11df9751fed2f1572fa518ab
Michael Hunger <github@jexp.de>
2012-12-18 03:05:04 +0100

    reflection for persistencemanager

--- src/main/java/org/neo4j/kernel/impl/core/Preloader.java
+++ src/main/java/org/neo4j/kernel/impl/core/Preloader.java
@@ -3,6 +3,7 @@ package org.neo4j.kernel.impl.core;
 import org.neo4j.graphdb.DependencyResolver;
 import org.neo4j.kernel.impl.persistence.PersistenceManager;
 
+import java.lang.reflect.Field;
 import java.util.concurrent.Callable;
 
 /**
@@ -17,15 +18,34 @@ public abstract class Preloader implements Callable<Integer> {
     protected final NodeManager nodeManager;
     protected final PersistenceManager persistenceManager;
     protected final long maxNodeId;
+    private static Field persistenceManagerField = getPersistenceManagerField();
+
+    private static Field getPersistenceManagerField() {
+        try {
+            final Field field = NodeManager.class.getDeclaredField("persistenceManager");
+            field.setAccessible(true);
+            return field;
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving NodeManager.persistenceManager", e);
+        }
+    }
 
     public Preloader(long segment, long minNodeId,
                      long nodeCount, int index, DependencyResolver dependencyResolver) {
-        this.persistenceManager = dependencyResolver.resolveDependency(PersistenceManager.class);
         this.nodeManager = dependencyResolver.resolveDependency(NodeManager.class);
+        this.persistenceManager = getPersistenceManager(nodeManager);
         this.segment = segment;
         this.minNodeId = minNodeId;
         this.maxNodeId = minNodeId + nodeCount;
         this.index = index;
+    }
+
+    private PersistenceManager getPersistenceManager(NodeManager nodeManager) {
+        try {
+            return (PersistenceManager) persistenceManagerField.get(nodeManager);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Integer call() throws Exception {
@@ -33,17 +53,17 @@ public abstract class Preloader implements Callable<Integer> {
         final long end = (index + 1) * segment;
         long fragment = segment / LOG_PARTS;
         long logAt = start + fragment;
-        System.out.printf("%2d. %s Loading from %10d up to %10d%n", index, getClass().getSimpleName(),start, end);
+        System.out.printf("%2d. %s Loading from %10d up to %10d%n", index, getClass().getSimpleName(), start, end);
         int count = 0;
         for (long id = start; id < end; id++) {
-            count+=doLoad(id);
+            count += doLoad(id);
             if (id > logAt) {
                 System.out.printf("%2d. %3d%%%n", index, 100 / LOG_PARTS * (id - start) / fragment);
                 System.out.flush();
                 logAt += fragment;
             }
         }
-        System.out.printf("%2d. %s 100%% Done, loaded %d %n", index,getClass().getSimpleName(),count);
+        System.out.printf("%2d. %s 100%% Done, loaded %d %n", index, getClass().getSimpleName(), count);
         return count;
     }
 
